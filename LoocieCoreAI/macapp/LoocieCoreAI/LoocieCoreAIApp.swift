d362147d352b3f0c719bb5f0f1566af8ee59b571
LoocieAIHQ <rex.a.carpenter2nd@gmail.com>
2026-03-05 21:55:13 -0500

    Stop app-owned engine on mac app termination

--- LoocieCoreAI/macapp/LoocieCoreAI/LoocieCoreAIApp.swift
+++ LoocieCoreAI/macapp/LoocieCoreAI/LoocieCoreAIApp.swift
@@ -23,4 +23,8 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         true
     }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        EngineManager.shared.stopOwnedEngine()
+    }
 }
